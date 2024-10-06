d01e23ea8445c70e7f842eafe57715c52ca41f3b
Nuttapong Phisitbutr <nuttp305@iteam.postbox.in.th>
2024-10-06 15:02:22 +0700

    Add project

---
+++ lib/home_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:nut_flutter_showcase/random_quote_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Nut's Flutter Showcase",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.grey,
+          bottom: const TabBar(
+            isScrollable: true,
+            tabAlignment: TabAlignment.startOffset,
+            tabs: [
+              Tab(icon: Icon(Icons.api), text: 'Fun With API'),
+              Tab(icon: Icon(Icons.web_asset_sharp), text: 'WebGl'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            _buildFunWithApiContent(context),
+            _buildWebglContent(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFunWithApiContent(BuildContext context) {
+    return Container(
+      color: Colors.black12,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+        child: ListView(
+          children: [
+            _buildMenuButtons(
+              buttons: [
+                _buildMenuButton(
+                  context,
+                  buttonName: 'Random Quote',
+                ),
+                _buildMenuButton(
+                  context,
+                  buttonName: 'Hobbies',
+                ),
+                _buildMenuButton(
+                  context,
+                  buttonName: 'Numbers',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWebglContent(BuildContext context) {
+    return Container(
+      color: Colors.black12,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+        child: ListView(
+          children: [
+            _buildMenuButtons(
+              buttons: [
+                _buildMenuButton(
+                  context,
+                  buttonName: 'AR',
+                ),
+                _buildMenuButton(
+                  context,
+                  buttonName: 'Game',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMenuHeader({
+    required String name,
+  }) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        name,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMenuButtons({
+    required List<Widget> buttons,
+  }) {
+    return Wrap(
+      spacing: 10,
+      runSpacing: 10,
+      children: buttons,
+    );
+  }
+
+  Widget _buildMenuButton(
+    BuildContext context, {
+    required String buttonName,
+  }) {
+    return ElevatedButton(
+      onPressed: () {
+        _navigateToScreen(context, buttonName);
+      },
+      child: Text(buttonName),
+    );
+  }
+
+  void _navigateToScreen(BuildContext context, String screenName) {
+    switch (screenName) {
+      case 'Random Quote':
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const RandomQuoteScreen()));
+        break;
+    }
+  }
+}
---
+++ lib/random_quote_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class RandomQuoteScreen extends StatefulWidget {
+  const RandomQuoteScreen({super.key});
+
+  @override
+  State<RandomQuoteScreen> createState() => _RandomQuoteScreenState();
+}
+
+class _RandomQuoteScreenState extends State<RandomQuoteScreen> {
+  String quote = '';
+  String author = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchRandomQuote();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Random Quote')),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: quote.isEmpty || author.isEmpty
+            ? const CircularProgressIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _buildQuoteCard(),
+                  const SizedBox(height: 100),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        quote = '';
+                        author = '';
+                      });
+
+                      fetchRandomQuote();
+                    },
+                    child: const Icon(Icons.navigate_next),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Widget _buildQuoteCard() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text(
+              quote,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 24.0),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              '-$author',
+              style:
+                  const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> fetchRandomQuote() async {
+    var response = await http.get(
+      Uri.parse('https://quotes15.p.rapidapi.com/quotes/random/'),
+      headers: {
+        'X-RapidAPI-Key': 'd6c331a93dmsh50acb261fb544bbp104233jsnf173aa315856',
+      },
+    );
+
+    setState(() {
+      Map valueMap = json.decode(response.body);
+      quote = valueMap['content'];
+      author = valueMap['originator']['name'];
+    });
+  }
+}
