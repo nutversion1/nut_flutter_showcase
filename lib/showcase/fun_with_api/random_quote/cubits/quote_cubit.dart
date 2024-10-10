98421ad834da412f9a63bf2067909938e56affe5
Nuttapong Phisitbutr <nutversion1@hotmail.com>
2024-10-10 10:16:30 +0700

    Update Quote Cubit to use Dio

--- lib/showcase/fun_with_api/random_quote/cubits/quote_cubit.dart
+++ lib/showcase/fun_with_api/random_quote/cubits/quote_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:dio/dio.dart';
 
 import '../models/quote.dart';
 import 'quote_states.dart';
@@ -8,30 +7,29 @@ import 'quote_states.dart';
 class QuoteCubit extends Cubit<BaseState> {
   QuoteCubit() : super(const BaseInitialState());
 
+  final dio = Dio(
+    BaseOptions(
+      headers: {
+        'X-RapidAPI-Key': 'd6c331a93dmsh50acb261fb544bbp104233jsnf173aa315856'
+      },
+    ),
+  );
+
   Future<void> fetchRandomQuote() async {
     try {
       emit(const BaseLoadingState());
 
       var url = 'https://quotes15.p.rapidapi.com/quotes/random/';
-      var headers = {
-        'X-RapidAPI-Key': 'd6c331a93dmsh50acb261fb544bbp104233jsnf173aa315856',
-      };
-
-      var response = await http.get(
-        Uri.parse(url),
-        headers: headers,
-      );
-
-      print('res: ${response.statusCode}');
+      var response = await dio.get(url);
 
       if (response.statusCode == 200) {
-        Map valueMap = json.decode(response.body);
+        Map valueMap = response.data;
         var quote = valueMap['content'];
         var author = valueMap['originator']['name'];
 
         emit(BaseCompletedState(data: Quote(quote, author)));
       } else {
-        emit(BaseErrorState(errorMessage: 'API Error: ${response.body}'));
+        emit(BaseErrorState(errorMessage: 'API Error: ${response.data}'));
       }
     } catch (e) {
       emit(BaseErrorState(errorMessage: 'Fetching Error: $e'));
